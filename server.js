41ed6262a2b8e1c95a64cd4b49693839b84992e8
Bryan Prinz <eric_scapini@estudante.sesisenai.org.br>
2025-04-17 06:07:43 -0300

    menu Inicial

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ const io = new Server(server);
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/home', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'home.html'));
 });
 

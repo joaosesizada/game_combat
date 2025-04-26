466fd074d8bc272ef12a45863a16d292c63dd1f8
João Vitor Santos <joaovsantos183@gmail.com>
2025-04-26 19:17:47 -0300

    Manual finalizado

--- public/js/manual.js
+++ public/js/manual.js
@@ -1,1 +1,6 @@
+function mostrarPagina(id) {
+    const paginas = document.querySelectorAll('.page')
+    paginas.forEach(pagina => pagina.classList.remove('ativa'))
+    document.getElementById(id).classList.add('ativa')
+}
 
